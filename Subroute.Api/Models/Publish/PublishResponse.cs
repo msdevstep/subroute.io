4074508cec40132cfd4160bc2a8102671b29082a
Dylan Vester <dvester@odatahq.com>
2016-04-08 12:26:28 -0400

    Removed publish/ from .gitignore file because it was excluding a necessary file from the models/publish folder in the Subroute.Api project.

---
+++ Subroute.Api/Models/Publish/PublishResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Subroute.Api.Models.Routes;
+using Subroute.Core.Compiler;
+using Subroute.Core.Data;
+
+namespace Subroute.Api.Models.Publish
+{
+    public class PublishResponse
+    {
+        public RouteResponse Route { get; set; }
+        public CompilationResult Compilation { get; set; }
+    }
+}
