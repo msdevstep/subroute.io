4659931441a1ef10381b9df1a29425c3eb96d319
Dylan Vester <dvester@subroute.io>
2016-04-18 22:57:17 -0400

    Added the newest version of font awesome to the project with bower and updated the reference to it with index.html.  I also added the route back to the documentation page. I dumped some exmaple markdown syntax to get an idea of how things will look. This will also help me come up with the CSS styles for common use cases. I added a button to the main navigation menu to link to documentation.

--- Subroute.App/app/viewmodels/container.js
+++ Subroute.App/app/viewmodels/container.js
@@ -3,6 +3,7 @@
     .makeRelative({ moduleId: 'viewmodels' })
     .map([
         { route: ['', 'routes/:action(/:uri)'], moduleId: 'routes', title: 'Routes', nav: true },
+        { route: 'documentation', moduleId: 'documentation', title: 'Documentation', nav: true },
         { route: 'gallery', moduleId: 'gallery', title: 'Gallery', nav: true }
     ]).buildNavigationModel();
 
